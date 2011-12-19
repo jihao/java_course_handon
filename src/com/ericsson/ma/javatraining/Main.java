065110e8a9e37dc1cf5ae6b994417fbaa25ed3ee
Honnix Liang <hxliang1982@gmail.com>
2011-12-19 17:35:35 +0800

    add main class to show -cp option

---
+++ src/com/ericsson/ma/javatraining/Main.java
@@ -0,0 +1,9 @@
+package com.ericsson.ma.javatraining;
+
+import com.ericsson.ma.javatraining.resource.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Resource resource = new Designer();
+    }
+}
