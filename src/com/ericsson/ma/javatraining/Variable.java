3ef338425f9c3b2d49d788bfca1269bb82700b1c
Honnix Liang <hxliang1982@gmail.com>
2011-12-19 17:51:18 +0800

    default value

---
+++ src/com/ericsson/ma/javatraining/Variable.java
@@ -0,0 +1,17 @@
+package com.ericsson.ma.javatraining;
+
+public class Variable {
+    private int a;
+
+    private float b;
+
+    private boolean c;
+
+    public static void main(String[] args) {
+        Variable variable = new Variable();
+
+        System.out.println(variable.a);
+        System.out.println(variable.b);
+        System.out.println(variable.c);
+    }
+}
