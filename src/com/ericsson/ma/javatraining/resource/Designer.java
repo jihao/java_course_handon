6f7fba156730921c142c7616eb5424693dfc211f
Honnix Liang <hxliang1982@gmail.com>
2012-01-11 18:13:27 +0800

    kjklj

--- src/com/ericsson/ma/javatraining/resource/Designer.java
+++ src/com/ericsson/ma/javatraining/resource/Designer.java
@@ -4,32 +4,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Designer extends Resource {
-    public Designer() {
-        super();
-    }
-
-    public Designer(String name) {
-        this.name = name;
-    }
-
     @Override
-    @SuppressWarnings("unchecked")
-    public String work() {
-        List todo = new ArrayList();
-        todo.add("design");
-        todo.add("reading");
+    public void work() {
         System.out.println(todo);
-        return "working";
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        Resource resource = (Resource) other;
-        return id.equals(resource.id) && name.equals(resource.name) && gender.equals(resource.gender) && unit.equals(resource.unit);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Designer:%s,%s,%s,%s", id, name, gender, unit);
     }
 }
